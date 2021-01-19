84ea5553dc474b8c91f83339d85f9971abd0e617
vishnuram <vishnumurali9762@gmail.com>
2021-01-19 10:40:00 +0530

    First Sample to print

---
+++ first.py
@@ -0,0 +1,16 @@
+print('test1')
+print('test2')
+print('test3')
+
+word1='one'
+word2='two'
+
+a=10;
+b=20;
+
+print(a+b)
+
+print(word1+word2)
+
+# This is will give error string cannot concatenate with int
+# print(word1+a)
